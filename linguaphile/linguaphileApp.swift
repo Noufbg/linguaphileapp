bc4a9f4a0b2b91a8250a551890092d24787fb5d7
nouf <noufbg@hotmail.com>
2023-01-11 17:44:53 +0300

    Initial Commit

---
+++ linguaphile/linguaphileApp.swift
@@ -0,0 +1,17 @@
+//
+//  linguaphileApp.swift
+//  linguaphile
+//
+//  Created by Nouf almandeel on 18/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct linguaphileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
